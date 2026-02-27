7cf03a7c9e5347c185778f688a8dfb2b1de27b40
AleksandarKaraganchev <alexsandarkaraganche@gmail.com>
2026-02-27 17:44:54 +0200

    Identity-User roles updates

--- PowerFIt/obj/Debug/net9.0/PowerFIt.AssemblyInfo.cs
+++ PowerFIt/obj/Debug/net9.0/PowerFIt.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PowerFIt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7315c5b3afa2c2187535f8365f31d43931144b00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde085c797c32c884bec00e5c482d156aa2a4b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("PowerFIt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PowerFIt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
