2736b721c2c1012a5080d1e3daab7ff4d35f659b
AleksandarKaraganchev <alexsandarkaraganche@gmail.com>
2026-03-24 10:25:37 +0200

    Little layout changes

--- PowerFIt/obj/Debug/net9.0/PowerFIt.AssemblyInfo.cs
+++ PowerFIt/obj/Debug/net9.0/PowerFIt.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PowerFIt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66cb4af272fdb546401315d2693ad8a5ed887cad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97966f7eed7c6c554951d572819ef17989849956")]
 [assembly: System.Reflection.AssemblyProductAttribute("PowerFIt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PowerFIt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
