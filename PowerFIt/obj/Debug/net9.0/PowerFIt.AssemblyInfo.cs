34e1762bad19af0785e77fc5a322272cbfcfe342
AleksandarKaraganchev <alexsandarkaraganche@gmail.com>
2026-05-09 13:17:54 +0300

    Small text changes

--- PowerFIt/obj/Debug/net9.0/PowerFIt.AssemblyInfo.cs
+++ PowerFIt/obj/Debug/net9.0/PowerFIt.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PowerFIt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1ee384bfdd30d741b20eb09a98a5461d0b8c0fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+397237fd6fb8889cc7492604cf251d6cf9c6b9df")]
 [assembly: System.Reflection.AssemblyProductAttribute("PowerFIt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PowerFIt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
